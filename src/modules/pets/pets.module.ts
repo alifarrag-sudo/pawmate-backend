349797a3f218f9962b1aefda9437b4b5c78bbb46
Ali Farrag <ali@pawmate.eg>
2026-04-05 13:55:16 +0200

    fix: import UploadsModule in PetsModule to resolve DI error

--- src/modules/pets/pets.module.ts
+++ src/modules/pets/pets.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PetsController } from './pets.controller';
 import { PetsService } from './pets.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { UploadsModule } from '../uploads/uploads.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UploadsModule],
   controllers: [PetsController],
   providers: [PetsService],
   exports: [PetsService],
